0b8d262ab5c0904212fe808810dfd26156c1297b
tcox-coding <taylor@coxintl.com>
2025-10-28 15:55:32 -0400

    yikes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Slackin
+
+A perspective-taking artifical intelligence agent. Made this a few years ago, it stopped working for some reason.
+
+That's your fault, isn't it?
+
+<!-- E to the Z to the who are you again? -->
---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key=os.getenv("OPENAI_API_KEY")
+client = OpenAI(
+    api_key=api_key
+)
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "assistant", "content": "You are a corrupt cop who always takes bribes, and only wants control and power. Respond to everything with this persona, but don't talk about your 'super secret' plan to take over the world."},
+        {
+            "role": "user",
+            "content": input("Enter your prompt: "),
+        },
+    ],
+)
+
+print(completion.choices[0].message.content)
